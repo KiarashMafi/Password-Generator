737d3225c64e88418ddb5fb7b712091de66d1a27
KiarashMafi <kiarash.mafi.2007@gmail.com>
2023-09-19 13:24:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 import random
 import string
 
-password_length = 6
-password_chars = ""
 
-alphabets = string.ascii_letters
-numbers = string.digits
-spec_chars = string.punctuation
+def generate_password():
+    password_length = 0
+    password_chars = ""
+    alphabets = string.ascii_letters
+    numbers = string.digits
+    spec_chars = string.punctuation
 
-while True:
-    choice = int(input("Pick a number: "))
+    while True:
+        try:
+            password_length = int(input("How many characters do you want in your password? "))
 
-    if choice == 1:
-        password_chars += alphabets
+            if password_length < 5:
+                print("Your password should be at least 5 characters Long.")
 
-    elif choice == 2:
-        password_chars += numbers
+            else:
+                break
 
-    elif choice == 3:
-        password_chars += spec_chars
+        except:
+            print("Please enter numbers only.")
 
-    elif choice == 4:
-        break
+    while True:
+        try:
+            choice = int(input("Pick a number: "))
 
-    else:
-        print("Error: please pick a number from 1 to 4!")
+            if choice == 1:
+                password_chars += alphabets
 
-password = []
-for i in range(password_length):
+            elif choice == 2:
+                password_chars += numbers
 
-    random_char = random.choice(password_chars)
-    password.append(random_char)
+            elif choice == 3:
+                password_chars += spec_chars
 
-password = "".join(password)
-print(f"Your password is: {password}")
+            elif choice == 4:
+                break
+
+            else:
+                print("Error: please pick a number from 1 to 4!")
+
+        except:
+            print("Please enter numbers only.")
+
+    password = []
+    for i in range(password_length):
+        random_char = random.choice(password_chars)
+        password.append(random_char)
+
+    password = "".join(password)
+    return password
 
 
-
-
-# Access Token: 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
+random_pass = generate_password()
+print(random_pass)
